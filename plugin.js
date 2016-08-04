8ee066ce98a2a95cf41d5c11788e1a5e5c80aceb
James Sumners <james.sumners@gmail.com>
2016-08-04 14:57:55 -0400

    Update to standardjs.com style

--- plugin.js
+++ plugin.js
@@ -1,46 +1,46 @@
-'use strict';
+'use strict'
 
-const Joi = require('joi');
-const introduce = require('introduce')(__dirname);
+const Joi = require('joi')
+const introduce = require('introduce')(__dirname)
 
 const configSchema = Joi.object().keys({
   encryptionKey: Joi.string().min(32).required()
-});
+})
 
-let log;
-let mongoose;
-let config;
-let server;
+let log
+let mongoose
+let config
+let server
 
-module.exports.name = 'clearpass';
-module.exports.plugin = function plugin(settings, context) {
-  log = context.logger;
-  mongoose = context.dataSources.mongoose;
+module.exports.name = 'clearpass'
+module.exports.plugin = function plugin (settings, context) {
+  log = context.logger
+  mongoose = context.dataSources.mongoose
 
   if (!mongoose) {
-    return Promise.reject(new Error('mongoose data source is required'));
+    return Promise.reject(new Error('mongoose data source is required'))
   }
 
-  config = Joi.validate(settings, configSchema);
+  config = Joi.validate(settings, configSchema)
   if (config.error) {
-    return Promise.reject(new Error(config.error));
+    return Promise.reject(new Error(config.error))
   }
 
-  return Promise.resolve({});
-};
+  return Promise.resolve({})
+}
 
-module.exports.postInit = function postInit(context) {
-  server = context.server;
+module.exports.postInit = function postInit (context) {
+  server = context.server
 
-  const dao = introduce('lib/dao')(log, mongoose, config.value.encryptionKey);
-  const interceptor = introduce('lib/interceptor')(log, dao);
+  const dao = introduce('lib/dao')(log, mongoose, config.value.encryptionKey)
+  const interceptor = introduce('lib/interceptor')(log, dao)
 
-  const routes = introduce('lib/routes')(log, dao);
-  server.route(routes);
+  const routes = introduce('lib/routes')(log, dao)
+  server.route(routes)
 
   return Promise.resolve({
     hooks: {
       preAuth: interceptor.hook.bind(interceptor)
     }
-  });
-};
+  })
+}
--- test/index.js
+++ test/index.js
@@ -1,10 +1,11 @@
-'use strict';
+'use strict'
+/* eslint-env node, mocha */
 
-const expect = require('chai').expect;
+const expect = require('chai').expect
 
-suite('tests');
+suite('tests')
 
-test('a simple test', function simple(done) {
-  expect(1).to.equal(1);
-  done();
-});
+test('a simple test', function simple (done) {
+  expect(1).to.equal(1)
+  done()
+})
