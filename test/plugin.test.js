9b5bf1a2cf1b16cc2efea9334172cc7de9973d53
James Sumners <james.sumners@gmail.com>
2018-01-08 10:06:57 -0500

    Add failing REST API test

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -111,61 +111,125 @@ test('works with a redis connection', (t) => {
   })
 })
 
-test('works with a mongo connection', (t) => {
-  t.plan(3)
-  const server = fastify()
+test('mongodb based tests', (t) => {
+  const mockgo = require('mockgo')
+  t.tearDown((done) => mockgo.shutDown(done))
 
-  let port = 0
-  const mocko = require('mockgo')
-  t.tearDown((done) => mocko.shutDown(done))
-  mocko.getConnection((err, conn) => {
-    if (err) t.threw(err)
+  t.test('works with a mongo connection', (t) => {
+    t.plan(3)
+    const server = fastify()
 
-    server
-      .register(require('fastify-mongodb'), {client: conn})
-      .decorate('jscasHooks', {})
-      .decorate('registerHook', function (name, fn) {
-        this.jscasHooks[name] = fn
-      })
-      .register(plugin, {
-        encryptionKey: key,
-        authKeys: ['123456']
-      })
-
-    server.listen(0, (err) => {
+    let port = 0
+    mockgo.getConnection((err, conn) => {
       if (err) t.threw(err)
-      server.server.unref()
-      port = server.server.address().port
 
-      server.jscasHooks.preAuth({username: 'foo', password: 'bar'})
-        .then((result) => {
-          t.ok(result)
-          doRequest()
+      server
+        .register(require('fastify-mongodb'), {client: conn})
+        .decorate('jscasHooks', {})
+        .decorate('registerHook', function (name, fn) {
+          this.jscasHooks[name] = fn
         })
-        .catch(t.threw)
+        .register(plugin, {
+          encryptionKey: key,
+          authKeys: ['123456']
+        })
+
+      server.listen(0, (err) => {
+        if (err) t.threw(err)
+        server.server.unref()
+        port = server.server.address().port
+
+        server.jscasHooks.preAuth({username: 'foo', password: 'bar'})
+          .then((result) => {
+            t.ok(result)
+            doRequest()
+          })
+          .catch(t.threw)
+      })
     })
+
+    const doRequest = () => sget(
+      {
+        url: `http://127.0.0.1:${port}/clearpass/foo/credentials`,
+        headers: {
+          'authorization': 'bearer 123456'
+        }
+      },
+      (err, res, data) => {
+        if (err) t.threw(err)
+        t.is(res.statusCode, 200)
+        let d = ''
+        res.on('data', (data) => {
+          d = d + data.toString()
+        })
+        res.on('end', () => {
+          t.strictDeepEqual(JSON.parse(d), {
+            username: 'foo',
+            password: 'bar'
+          })
+        })
+      }
+    )
   })
 
-  const doRequest = () => sget(
-    {
-      url: `http://127.0.0.1:${port}/clearpass/foo/credentials`,
-      headers: {
-        'authorization': 'bearer 123456'
-      }
-    },
-    (err, res, data) => {
+  t.test('returns error for expired credentials', (t) => {
+    t.plan(3)
+    const server = fastify()
+
+    let port = 0
+    mockgo.getConnection((err, conn) => {
       if (err) t.threw(err)
-      t.is(res.statusCode, 200)
-      let d = ''
-      res.on('data', (data) => {
-        d = d + data.toString()
-      })
-      res.on('end', () => {
-        t.strictDeepEqual(JSON.parse(d), {
-          username: 'foo',
-          password: 'bar'
+
+      server
+        .register(require('fastify-mongodb'), {client: conn})
+        .decorate('jscasHooks', {})
+        .decorate('registerHook', function (name, fn) {
+          this.jscasHooks[name] = fn
         })
+        .register(plugin, {
+          encryptionKey: key,
+          authKeys: ['123456'],
+          ttl: 100,
+          skew: 0
+        })
+
+      server.listen(0, (err) => {
+        if (err) t.threw(err)
+        server.server.unref()
+        port = server.server.address().port
+
+        server.jscasHooks.preAuth({username: 'foo', password: 'bar'})
+          .then((result) => {
+            t.ok(result)
+            setTimeout(doRequest, 1000)
+          })
+          .catch(t.threw)
       })
-    }
-  )
+    })
+
+    const doRequest = () => sget(
+      {
+        url: `http://127.0.0.1:${port}/clearpass/foo/credentials`,
+        headers: {
+          'authorization': 'bearer 123456'
+        }
+      },
+      (err, res, data) => {
+        if (err) t.threw(err)
+        t.is(res.statusCode, 500)
+        let d = ''
+        res.on('data', (data) => {
+          d = d + data.toString()
+        })
+        res.on('end', () => {
+          t.is(
+            JSON.parse(d).message,
+            'clearpass: failed to lookup credentials'
+          )
+        })
+      }
+    )
+  })
+
+  t.end()
 })
