ed5db65d29902d69477be7e99639b859439c5305
James Sumners <james.sumners@gmail.com>
2018-04-03 13:05:47 -0400

    Add unseal test and guard against non-Boom errors

--- test/redisdao.test.js
+++ test/redisdao.test.js
@@ -45,3 +45,13 @@ test('throws for purged credentials', {timeout: 2600}, (t) => {
     })
     .catch(t.threw)
 })
+
+test('throws when unable to unseal credentials', {timeout: 2600}, (t) => {
+  t.plan(1)
+  const dao = daoFactory({redis, encryptionKey: key, ttl: 100, skew: 1, log})
+  dao.storeCredentials('foo', 'bar')
+    .then(() => redis.set('clearpass%3Afoo', 'nope'))
+    .then(() => dao.getCredentials('foo'))
+    .then(() => t.fail('should not succeed'))
+    .catch((err) => t.match(err, /could not unseal credentials/))
+})
